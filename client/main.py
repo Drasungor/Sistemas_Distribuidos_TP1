9fc6ff3e1acdd2b3011abed931664ece11c9459a
Drasungor <aguscambiano@gmail.com>
2022-09-23 18:47:04 -0300

    Start client implementation

--- client/main.py
+++ client/main.py
@@ -6,10 +6,21 @@ import queue
 import json
 
 config_file_path = "config.json"
-config = (open(config_file_path, "r"))
+config = json.load(open(config_file_path, "r"))
+
+def send_file(files_paths_queue: mp.Queue):
+    connection_address = config["accepter_address"]
+    connection_port = config["accepter_port"]
+    process_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    process_socket.connect((connection_address, connection_port))
+    while #cola no lee None
+
 
 def main():
-    pass
+    files_paths = config["files_names"]
+
+    # Total amount of processes the client is composed of
+    processes_amount = min([config["processes_amount"], mp.cpu_count(), len(files_paths)])
 
 if __name__ == "__main__":
     main()
