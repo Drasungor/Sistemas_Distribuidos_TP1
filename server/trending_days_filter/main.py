e18920828e388b84de532ba2fb6d27ebd939323b
Drasungor <aguscambiano@gmail.com>
2022-10-01 00:15:29 -0300

    Finish first implementation of trending days filter

--- server/trending_days_filter/main.py
+++ server/trending_days_filter/main.py
@@ -13,31 +13,26 @@ local_config = config[cluster_type]
 class TrendingDaysFilter:
     def __init__(self):
         self.middleware = MOM(cluster_type, self.process_received_message)
-        self.aggregation_dict = {}
+        self.trending_days_amounts = {}
         self.received_eofs = 0
         
         previous_stage = local_config["receives_from"]
         self.previous_stage_size = config[previous_stage]["computers_amount"]
 
     def process_received_message(self, ch, method, properties, body):
-
         if method.routing_key == general_config["EOF_subscription_routing_key"]:
             self.received_eofs += 1
             if self.received_eofs == self.previous_stage_size:
                 self.middleware.send_final(body)
         else:
-            pass
-        # # line = json.loads(body)
-        # # date: str = line[general_config["trending_date_index"]]
-        # # view_count: str = line[general_config["views_index"]]
-        # # if not (date in self.aggregation_dict):
-        # #     self.aggregation_dict[date] = view_count
-        # # else:
-        # #     self.aggregation_dict[date] += view_count
-
-        # trending_amount["video.country"] += 1
-        # if trending_amount["video.country"] == 21 -> send video
-
+            line = json.loads(body)
+            video_id = line[general_config["indexes"]["video_id"]]
+            if not (video_id in self.trending_days_amounts):
+                self.trending_days_amounts[video_id] = 0
+            self.trending_days_amounts[video_id] += 1
+            current_trending_days_amount = self.trending_days_amounts[video_id]
+            if current_trending_days_amount == local_config["min_trending_days"]:
+                self.middleware.send(line)
 
     def start_received_messages_processing(self):
         self.middleware.start_received_messages_processing()
