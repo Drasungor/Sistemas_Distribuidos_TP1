7137dc1be8e12bb001593d57d6a68b4da8b1d705
Drasungor <aguscambiano@gmail.com>
2022-10-01 00:50:36 -0300

    .

--- server/thumbnails_downloader/main.py
+++ server/thumbnails_downloader/main.py
@@ -1,7 +1,7 @@
 import json
 from MOM.MOM import MOM
 
-cluster_type = "duplication_filter"
+cluster_type = "thumbnails_downloader"
 
 config_file_path = "/config/config.json"
 config = None
@@ -10,35 +10,29 @@ with open(config_file_path, "r") as config_file:
 general_config = config["general"]
 local_config = config[cluster_type]
 
-class DuplicationFilter:
+class MaxViewsDay:
     def __init__(self):
         self.middleware = MOM(cluster_type, self.process_received_message)
-        self.sent_videos = set()
+        self.max_views_date = (None, 0)
         self.received_eofs = 0
         
         previous_stage = local_config["receives_from"]
         self.previous_stage_size = config[previous_stage]["computers_amount"]
 
     def process_received_message(self, ch, method, properties, body):
-        line = json.loads(body)
-
-        if method.routing_key == general_config["EOF_subscription_routing_key"]:
+        if method.routing_key == general_config["EOF_subscription_routing_key"]: # TODO: check if this condition is correct
             self.received_eofs += 1
             if self.received_eofs == self.previous_stage_size:
                 self.middleware.send_final(None)
         else:
+            line = json.loads(body)
             video_id = line[general_config["indexes"]["video_id"]]
-            title = line[general_config["indexes"]["title"]]
-            category = line[general_config["indexes"]["category"]]
-            if not (video_id in self.received_eofs):
-                self.sent_videos.add(video_id)
-                self.middleware.send({ "type": cluster_type, "tuple": (video_id, title, category) })
 
     def start_received_messages_processing(self):
         self.middleware.start_received_messages_processing()
 
 def main():
-    wrapper = DuplicationFilter()
+    wrapper = MaxViewsDay()
     wrapper.start_received_messages_processing()
 
 if __name__ == "__main__":
