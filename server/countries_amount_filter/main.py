8b5a056e4ee9aed8412ebb4af626a59315a3aa8b
Drasungor <aguscambiano@gmail.com>
2022-10-06 11:51:50 -0300

    Delete comments

--- server/countries_amount_filter/main.py
+++ server/countries_amount_filter/main.py
@@ -16,11 +16,10 @@ class CountriesAmountFilter:
     def __init__(self):
         self.middleware = MOM(cluster_type, self.process_received_message)
         self.videos_countries = {}
-        self.countries_amount = None # TODO: we should receive the amount of countries after the client establishes a connection
+        self.countries_amount = None
         self.received_eofs = 0
 
         self.has_to_close = False
-        # self.is_processing_message = False
 
         previous_stage = local_config["receives_from"]
         self.previous_stage_size = config[previous_stage]["computers_amount"]
@@ -28,14 +27,12 @@ class CountriesAmountFilter:
         signal.signal(signal.SIGTERM, self.__handle_signal)
 
     def process_received_message(self, ch, method, properties, body):
-        # self.is_processing_message = True
         line = json.loads(body)
         if method.routing_key == general_config["general_subscription_routing_key"]:
             if line == None:
                 self.received_eofs += 1
                 if self.received_eofs == self.previous_stage_size:
                     self.middleware.send_general(None)
-                    # self.middleware.close()
                     self.has_to_close = True
             else:
                 self.countries_amount = line # Number
@@ -49,13 +46,11 @@ class CountriesAmountFilter:
             video_set.add(country)
             current_countries_amount = len(video_set)
             if (current_countries_amount == self.countries_amount) and (previous_countries_amount != current_countries_amount):
-                # self.middleware.send(video_id)
                 self.middleware.send(line)
 
         if self.has_to_close:
             self.middleware.close()
             logging.info("Closed MOM")
-        # self.is_processing_message = False
 
 
     def start_received_messages_processing(self):
@@ -64,11 +59,6 @@ class CountriesAmountFilter:
 
     def __handle_signal(self, *args): # To prevent double closing 
         self.has_to_close = True
-        # if self.is_processing_message:
-        #     self.has_to_close = True
-        # else:
-        #     self.middleware.close()
-        #     logging.info("Closed MOM")
 
 def main():
     # logging.basicConfig(
